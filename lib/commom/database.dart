6984abdfaa63c2bca0b0dcc989623ecc3bd438fb
Duong Son <duongson.dev@gmail.com>
2020-09-11 22:07:11 +0700

    database file

--- lib/commom/database.dart
+++ lib/commom/database.dart
@@ -1,10 +1,32 @@
+import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:sqflite/sqlite_api.dart';
-class DatabaseApp{
-  static Future<Database> getDatabaseApp() async{
-    return openDatabase(join(await getDatabasesPath(), "notes.db"),onCreate: (db, version){
-      db.execute("");
-    },version: 1);
+
+class DatabaseApp {
+  // table title
+  final String tblCategory = "Category";
+  final String tblNotes = "Notes";
+
+  static Future<Database> getDatabaseApp() async {
+    return openDatabase(join(await getDatabasesPath(), "notes.db"),
+        onCreate: (db, version) async {
+          await db.execute("CREATE TABLE $tblCategory("
+              "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+              "title TEXT,"
+              "color INTEGER)");
+          await db.execute("CREATE TABLE $tblNotes("
+              "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+              "title TEXT,"
+              "content TEXT,"
+              "create TEXT,"
+              "lastUpdated TEXT,"
+              "active INTEGER,"
+              "category INTEGER)");
+          await db.insert(tblCategory, {
+            'id': 0,
+            'name': 'All',
+            'color': Colors.orange.value
+          });
+        }, version: 1);
   }
 }
